ec63928e921c37c02aacdcac2e10e182f63409f2
Frederik Hjorslev Poulsen <frederik@hjorslev.com>
2020-04-23 23:15:24 +0200

    :tada: Initial commit

---
+++ src/PSQuotes/Public/Get-Quote.ps1
@@ -0,0 +1,17 @@
+﻿function Get-Quote {
+    [CmdletBinding()]
+    param (
+
+    )
+
+    process {
+        $RandomQuote = Invoke-RestMethod -Uri 'https://api.quotable.io/random'
+        if ([string]::IsNullOrWhiteSpace($RandomQuote.author)) {
+            $Quote = "$($RandomQuote.content)`v`t$([char]0x2014) Anonymous"
+            Write-Host -Object ($Quote -replace '^|\r?(\n)', ('$1    {0} ' -f [char]0x258c)) -ForegroundColor Magenta
+        } else {
+            $Quote = "$($RandomQuote.content)`v`t$([char]0x2014) $($RandomQuote.author)"
+            Write-Host -Object ($Quote -replace '^|\r?(\n)', ('$1    {0} ' -f [char]0x258c)) -ForegroundColor Magenta
+        }
+    } # Process
+} # Cmdlet
