95d776e71049a16aeda0b697dabf5f3f66dade45
Frederik Hjorslev Poulsen <frederik@hjorslev.com>
2020-04-24 12:28:07 +0200

    :sparkles: Add Get-Quote & Show-Quote
    First working concept. Get-Quote grabs a random quote whereas Show-Quote can be used to format it nicely.

---
+++ src/PSQuotes/Public/Get-Quote.ps1
@@ -0,0 +1,39 @@
+﻿function Get-Quote {
+    <#
+    .SYNOPSIS
+    Get an inspirational random quote.
+
+    .DESCRIPTION
+    Get an inspirational random quote from api.quotable.io.
+
+    .EXAMPLE
+    Get-Quote
+
+    .EXAMPLE
+    Get-Quote | Show-Quote
+
+    Formats the quote nicely.
+
+    .NOTES
+    Author: Frederik Hjorslev Poulsen
+
+    Modified: 24-04-2020
+    #>
+
+    [CmdletBinding()]
+    param (
+    )
+
+    process {
+        #region RandomQuote
+        $RandomQuote = Invoke-RestMethod -Uri 'https://api.quotable.io/random'
+        [PSCustomObject]@{
+            ID      = $RandomQuote._id
+            Tags    = $RandomQuote.tags
+            Content = $RandomQuote.content
+            Author  = $RandomQuote.author
+            Length  = $RandomQuote.length
+        }
+        #endregion RandomQuote
+    } # Process
+} # Cmdlet
--- src/PSQuotes/Public/Show-Quote.ps1
+++ src/PSQuotes/Public/Show-Quote.ps1
@@ -1,29 +1,96 @@
 ﻿function Show-Quote {
     <#
     .SYNOPSIS
-    Get a random quote.
+    Formats the quote nicely.
 
     .DESCRIPTION
-    Get a random quote from quotable.io
+    Cuts text to console-appropriate widths for viewing, where possible.
+
+    .PARAMETER Content
+    Enter a string.
+
+    .PARAMETER Author
+    Enter a string.
+
+    .PARAMETER QuoteColor
+    Choose a color for the quote.
 
     .EXAMPLE
-    Show-Quote
+    Show-Quote -Content 'Friendship at first sight, like love at first sight, is said to be the only truth.' -Author 'Herman Melville'
+
+    .EXAMPLE
+    Get-Quote | Show-Quote
+
+    .INPUTS
+    Show-Quote accepts pipeline input for the $Content parameter.
+
+    .OUTPUTS
+    Nothing. All output is sent only to the host / information stream.
 
     .NOTES
-    Author: fhp.it
-    #>
+    Author: Joel Sallow (https://github.com/vexx32/PSKoans)
+
+    This cmdlet is a rework of Write-ConsoleLine. The output is inspired by
+    how the Koans are displayed in the PSKoans project.
+
+    Modified by: Frederik Hjorslev Poulsen (https://github.com/hjorslev/PSQuotes)
+
+    Modified: 24-04-2020
+#>
+
 
     [CmdletBinding()]
-    param (
+    [OutputType([void])]
+    param(
+        [Parameter(Position = 0,
+            Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Content,
+
+        [Parameter(Position = 1,
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true)]
+        [string]$Author,
+
+        [Parameter(Mandatory = $false)]
+        [ConsoleColor]$QuoteColor
     )
 
-    process {
-        if ([string]::IsNullOrWhiteSpace($RandomQuote.author)) {
-            $Quote = "$($RandomQuote.content)`v`t$([char]0x2014) Anonymous"
-            Write-Host -Object ($Quote -replace '^|\r?(\n)', ('$1    {0} ' -f [char]0x258c)) -ForegroundColor Magenta
+    begin {
+        Write-Host -Object ""
+        $Prefix = "    $([char]0x258c)"
+
+        if ($QuoteColor) {
+            $Color = @{ ForegroundColor = [ConsoleColor]::$QuoteColor }
         } else {
-            $Quote = "$($RandomQuote.content)`v`t$([char]0x2014) $($RandomQuote.author)"
-            Write-Host -Object ($Quote -replace '^|\r?(\n)', ('$1    {0} ' -f [char]0x258c)) -ForegroundColor Magenta
+            $Color = @{ ForegroundColor = [ConsoleColor]::Magenta }
+        }
+
+        $Width = $host.UI.RawUI.WindowSize.Width - ($Prefix.Length + 2)
+    }
+
+    process {
+        # Ugly mode since width either not detectable or too small to bother
+        if ($Width -lt 20) {
+            Write-Host -Object $Content
+        } else {
+            $RemainingText = $Content.TrimEnd()
+
+            while ($RemainingText.Length -gt $Width) {
+                $CompleteLine = $RemainingText.Substring(0, $Width)
+                $TailFragment = ($CompleteLine -split "[- ]")[-1].Length
+                $BestFitLine = $CompleteLine.Substring(0, $CompleteLine.Length - $TailFragment).TrimEnd()
+
+                Write-Host -Object ($Prefix + $BestFitLine) @Color
+
+                $RemainingText = $RemainingText.Substring($CompleteLine.Length - $TailFragment)
+            }
+            if ([string]::IsNullOrWhiteSpace($Author)) {
+                Write-Host -Object "$($Prefix + $RemainingText)`v`t$([char]0x2014) Anonymous" @Color
+            } else {
+                Write-Host -Object "$($Prefix + $RemainingText)`v`t$([char]0x2014) $($Author)" @Color
+            }
         }
     } # Process
 } # Cmdlet
